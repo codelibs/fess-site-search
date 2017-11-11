67ddd4b2ade533ebcd974988b92892a1926c6370
Keiichi Watanabe <udon.watanabe@gmail.com>
2017-11-11 17:06:22 +0900

    Apply PEP8

--- app/backend.py
+++ app/backend.py
@@ -1,4 +1,8 @@
-import subprocess, os, hashlib, random, traceback
+import subprocess
+import os
+import hashlib
+import random
+import traceback
 from flask import redirect, url_for
 from flask import render_template
 from werkzeug.utils import secure_filename
@@ -7,66 +11,74 @@ from .generate_css import generate_css
 
 DEFAULT_VERSION = '11.4'
 
-def upload(form, file):
-  version = form.get('fess-version', DEFAULT_VERSION)
-  print(form)
 
-  if file.filename == '':
+def upload(form, file):
+    version = form.get('fess-version', DEFAULT_VERSION)
+    print(form)
+
+    if file.filename == '':
+        return render_template('index.html')
+
+    if file and is_css(file.filename):
+        base = secure_filename(file.filename)[:-4]
+        hash_str = rand_hash()
+        fname = '{}_{}_{}'.format(base, hash_str, version.replace('.', '_'))
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], fname + '.css'))
+        print('Upload: {}.css'.format(fname))
+        return run_webpack(fname, version)
+
     return render_template('index.html')
 
-  if file and is_css(file.filename):
-    base = secure_filename(file.filename)[:-4]
-    hash_str = rand_hash()
-    fname = "{}_{}_{}".format(base, hash_str, version.replace('.', '_'))
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], fname + ".css"))
-    print("Upload: {}.css".format(fname))
-    return run_webpack(fname, version)
-
-  return render_template('index.html')
 
 def wizard(form):
-  print('wizard!!')
-  version = form.get('fess-version', DEFAULT_VERSION)
-  fname = 'wizard_{}_{}'.format(rand_hash(), version.replace('.', '_'))
-  if generate_css(form, fname):
-    return run_webpack(fname, version)
-  else:
-    return render_template('index.html')
+    print('wizard!!')
+    version = form.get('fess-version', DEFAULT_VERSION)
+    fname = 'wizard_{}_{}'.format(rand_hash(), version.replace('.', '_'))
+    if generate_css(form, fname):
+        return run_webpack(fname, version)
+    else:
+        return render_template('index.html')
+
 
 def run_webpack(fname, version):
-  my_env = os.environ.copy()
-  jsfile = 'fess-ss-{}.min.js'.format(fname)
+    my_env = os.environ.copy()
+    jsfile = 'fess-ss-{}.min.js'.format(fname)
 
-  my_env["INPUT_CSS_PATH"] = "{}/{}.css".format(app.config['UPLOAD_FOLDER'],fname)
-  my_env["OUTPUT_JS_FILENAME"] = jsfile
+    my_env['INPUT_CSS_PATH'] = '{}/{}.css'.format(app.config['UPLOAD_FOLDER'], fname)
+    my_env['OUTPUT_JS_FILENAME'] = jsfile
 
-  print("generate_js: {} -> {}".format(my_env["INPUT_CSS_PATH"], my_env["OUTPUT_JS_FILENAME"]))
+    print('generate_js: {} -> {}'.format(my_env['INPUT_CSS_PATH'], my_env['OUTPUT_JS_FILENAME']))
 
-  try:
-    (cwd, cmd) = get_command(version)
-    print('Command: {}'.format(cmd))
-    proc = subprocess.Popen(cmd, env=my_env, cwd=cwd)
-    print('Redirect > /demo/{}'.format(fname))
-    return redirect(url_for('demo', fname=fname))
-  except:
-    print("Fail to generate {}".format(jsfile))
-    traceback.print_exc()
-    return redirect(url_for('index'))
+    try:
+        (cwd, cmd) = get_command(version)
+        print('Command: {}'.format(cmd))
+        proc = subprocess.Popen(cmd, env=my_env, cwd=cwd)
+        print('Redirect > /demo/{}'.format(fname))
+        return redirect(url_for('demo', fname=fname))
+    except:
+        print('Fail to generate {}'.format(jsfile))
+        traceback.print_exc()
+        return redirect(url_for('index'))
 
 ###########
 # Utilities
 ###########
+
+
 def rand_hash():
-  hash = hashlib.sha256(str(random.getrandbits(256)).encode('utf-8')).hexdigest()
-  return hash[:10]
+    hash = hashlib.sha256(
+        str(random.getrandbits(256)).encode('utf-8')).hexdigest()
+    return hash[:10]
+
 
 def is_css(filename):
-  if '.' in filename:
-    ext = filename.rsplit('.', 1)[1].lower()
-    return ext == 'css'
-  return False
+    if '.' in filename:
+        ext = filename.rsplit('.', 1)[1].lower()
+        return ext == 'css'
+    return False
+
 
 def get_command(version):
-  cwd = os.path.join(app.instance_path, '../fss/{}'.format(version))
-  cmd = '{0}/node_modules/.bin/webpack --config {0}/webpack.config.js'.format(cwd)
-  return (cwd, cmd.split())
+    cwd = os.path.join(app.instance_path, '../fss/{}'.format(version))
+    cmd = '{0}/node_modules/.bin/webpack --config {0}/webpack.config.js'.format(cwd)
+    return (cwd, cmd.split())
--- app/views.py
+++ app/views.py
@@ -3,75 +3,77 @@ from flask import g
 from flask import request, redirect, url_for, render_template
 from flask import jsonify, abort, make_response
 from markdown import markdown
-
 from .app import app, babel
 from .backend import upload, wizard
+
 
 @app.before_request
 @babel.localeselector
 def detect_user_language():
-  g.language = request.accept_languages.best_match(['en', 'ja'])
+    g.language = request.accept_languages.best_match(['en', 'ja'])
 
-# Top Page
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  if request.method == 'POST':
-    if 'file' in request.files:
-      return upload(request.form, request.files['file'])
-    else:
-      return wizard(request.form)
+    """index page"""
+    if request.method == 'POST':
+        if 'file' in request.files:
+            return upload(request.form, request.files['file'])
+        else:
+            return wizard(request.form)
 
-  return render_template('index.html')
+    return render_template('index.html')
 
-# Demo Page
+
 @app.route('/demo/<fname>')
 def demo(fname):
-  my_dic = {}
-  my_dic['js_path'] = '/generates/fess-ss-{}.min.js'.format(fname)
-  my_dic['js_file'] = 'fess-ss-{}.min.js'.format(fname)
-  my_dic['search_src'] = '/search/{}?q=test'.format(fname)
-  print(my_dic['search_src'])
-  return render_template('demo.html', message=my_dic)
+    """demo page"""
+    my_dic = {}
+    my_dic['js_path'] = '/generates/fess-ss-{}.min.js'.format(fname)
+    my_dic['js_file'] = 'fess-ss-{}.min.js'.format(fname)
+    my_dic['search_src'] = '/search/{}?q=test'.format(fname)
+    print(my_dic['search_src'])
+    return render_template('demo.html', message=my_dic)
 
-# Search Result for Preview
+
 @app.route('/search/')
 def preview():
-  my_dic = {}
-  my_dic['js_path'] = '/static/fss/11.4/fess-ss.min.js'
-  my_dic['page_path'] = '/search/'
-  return render_template('search.html', message=my_dic)
+    """search page used in preview"""
+    my_dic = {}
+    my_dic['js_path'] = '/static/fss/11.4/fess-ss.min.js'
+    my_dic['page_path'] = '/search/'
+    return render_template('search.html', message=my_dic)
 
-# Search Result Frame in Demo page
+
 @app.route('/search/<fname>')
 def search(fname):
-  my_dic = {}
-  my_dic['js_path'] = '/generates/fess-ss-{}.min.js'.format(fname)
-  my_dic['page_path'] = '/search/{}'.format(fname)
-  return render_template('search.html', message=my_dic)
+    """search frame in demo page"""
+    my_dic = {}
+    my_dic['js_path'] = '/generates/fess-ss-{}.min.js'.format(fname)
+    my_dic['page_path'] = '/search/{}'.format(fname)
+    return render_template('search.html', message=my_dic)
 
-# Search Results in Frame
+
 @app.route('/docs/manual')
 def manual():
-  lang = g.language if g.language is not None else 'en'
-  path = os.path.join(app.config['DOCS_FOLDER'], 'user-manual.{}.md'.format(lang))
-  md_file = open(path, mode='r', encoding='utf-8')
-  md_str = md_file.read()
-  md_file.close()
-  extensions = ['gfm']
-  html = markdown(md_str, extensions=extensions)
+    """manual page"""
+    lang = g.language if g.language is not None else 'en'
+    path = os.path.join(app.config['DOCS_FOLDER'], 'user-manual.{}.md'.format(lang))
+    md_file = open(path, mode='r', encoding='utf-8')
+    md_str = md_file.read()
+    md_file.close()
+    extensions = ['gfm']
+    html = markdown(md_str, extensions=extensions)
+    my_dic = {}
+    my_dic['markdown_content'] = html
+    return render_template('manual.html', message=my_dic)
 
-  my_dic = {}
-  my_dic['markdown_content'] = html
-  return render_template('manual.html', message=my_dic)
 
-###########
-# API
-###########
-# Check if 'fess-ss-<fname>.min.js' exists
 @app.route('/api/check_js/<fname>', methods=['GET'])
 def check_js(fname):
-  jsfile = 'fess-ss-{}.min.js'.format(fname)
-  path = os.path.join(app.config['DOWNLOAD_FOLDER'], jsfile)
-  if os.path.exists(path):
-    return make_response(jsonify({'result': True}))
-  return make_response(jsonify({'result': False}))
+    """API: check if 'fess-ss-<fname>.min.js' exists"""
+    jsfile = 'fess-ss-{}.min.js'.format(fname)
+    path = os.path.join(app.config['DOWNLOAD_FOLDER'], jsfile)
+    if os.path.exists(path):
+        return make_response(jsonify({'result': True}))
+    return make_response(jsonify({'result': False}))
